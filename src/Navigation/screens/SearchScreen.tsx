c8c90aaa1fc6c41c358e02317ac7c9be145598a4
ozakione <29860391+OzakIOne@users.noreply.github.com>
2022-11-24 20:00:38 +0100

    clean with neo

--- src/Navigation/screens/SearchScreen.tsx
+++ src/Navigation/screens/SearchScreen.tsx
@@ -1,52 +1,41 @@
-import * as React from 'react';
 import { View, Text } from 'react-native';
-import { useState, useDeferredValue } from 'react';
+import { useState } from 'react';
 import { Searchbar, List } from 'react-native-paper';
-import useThrottle from 'react-use/lib/useThrottle';
-import useThrottleFn from 'react-use/lib/useThrottleFn';
+import debounce from 'lodash.debounce';
 
 // ! fix all the types
 
+const apiKey = 'e5591759270b4ad0c89059888603757d';
+
+const myfetch = (search: string) =>
+  debounce(
+    fetch(
+      `https://ws.audioscrobbler.com/2.0/?method=album.search&album=%27${search}'&api_key=${apiKey}&format=json`,
+    ).then((response: any) => response.json()),
+    1000,
+  );
+
+const myfetch2 = debounce(myfetch, 1000);
+
 export default function SearchScreen() {
   const [fmData, setFmData] = useState<any>({});
-  const [searchTerm, setSearchTerm] = useState<String>('');
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const apiKey = 'e5591759270b4ad0c89059888603757d';
+  const handleInput = (text: string) => {
+    setSearchTerm(text);
 
-  const handleInput = (text: string) => setSearchTerm(text);
-
-  const handleFetch = async () => {
-    console.log('FETCHING');
-
-    const rawData = await fetch(
-      `https://ws.audioscrobbler.com/2.0/?method=album.search&album=%27${throttleValue}'&api_key=${apiKey}&format=json`,
-    );
-    const json = await rawData.json();
-    setFmData(json);
+    myfetch(text).then((data: any) => {
+      setFmData(data);
+    });
   };
-  const throttleValue = useThrottleFn(handleFetch, 500, []);
-
-  const handleFetch2 = useThrottleFn(
-    async () => {
-      console.log('FETCHING');
-
-      const rawData = await fetch(
-        `https://ws.audioscrobbler.com/2.0/?method=album.search&album=%27${throttleValue}'&api_key=${apiKey}&format=json`,
-      );
-      const json = await rawData.json();
-      setFmData(json);
-    },
-    500,
-    [],
-  );
 
   return (
     <View>
       <Text style={{ fontSize: 26, fontWeight: 'bold' }}>Search Screen</Text>
       <Searchbar
         placeholder="Search"
-        value={searchTerm as string}
-        onChangeText={handleFetch2}
+        value={searchTerm}
+        onChangeText={handleInput}
       />
       <List.Section>
         <List.Subheader>Résultats</List.Subheader>
