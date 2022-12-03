9d529b9964850faa99cdc60e5879325d70958d2b
ozaki <29860391+OzakIOne@users.noreply.github.com>
2022-12-03 20:48:15 +0100

    working debounce

--- src/Navigation/screens/SearchScreen.tsx
+++ src/Navigation/screens/SearchScreen.tsx
@@ -1,54 +1,73 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  ToastAndroid,
+  FlatList,
+  StyleSheet,
+  Image,
+} from 'react-native';
 import { useState } from 'react';
-import { Searchbar, List } from 'react-native-paper';
+import { Searchbar, Button } from 'react-native-paper';
 import debounce from 'lodash.debounce';
-
-// ! fix all the types
 
 const apiKey = 'e5591759270b4ad0c89059888603757d';
 
 const myfetch = (search: string) =>
-  debounce(
-    fetch(
-      `https://ws.audioscrobbler.com/2.0/?method=album.search&album=%27${search}'&api_key=${apiKey}&format=json`,
-    ).then((response: any) => response.json()),
-    1000,
-  );
+  fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=album.search&album=${search}&api_key=${apiKey}&format=json`,
+  ).then((res) => res.json());
 
-const myfetch2 = debounce(myfetch, 1000);
+const Item = ({ title, image }: { title: string; image: string }) => (
+  <View style={styles.item}>
+    <Image source={{ uri: { image } }} style={{ width: 50, height: 50 }} />
+    <Text style={styles.title}>{title}</Text>
+    <Button
+      icon="plus"
+      onPress={() =>
+        ToastAndroid.show('Button plus pressed', ToastAndroid.SHORT)
+      }
+      children=""
+    />
+  </View>
+);
 
 export default function SearchScreen() {
-  const [fmData, setFmData] = useState<any>({});
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [fmData, setFmData] = useState({});
 
-  const handleInput = (text: string) => {
-    setSearchTerm(text);
-
-    myfetch(text).then((data: any) => {
-      setFmData(data);
-    });
-  };
+  const renderItem = ({ item }: { item: any }) => (
+    <Item title={item.name} image={item.image[0]['#text']} />
+  );
 
   return (
-    <View>
-      <Text style={{ fontSize: 26, fontWeight: 'bold' }}>Search Screen</Text>
+    <View style={styles.container}>
       <Searchbar
-        placeholder="Search"
-        value={searchTerm}
-        onChangeText={handleInput}
+        onChangeText={debounce((e) => myfetch(e).then(setFmData), 300)}
       />
-      <List.Section>
-        <List.Subheader>Résultats</List.Subheader>
-        {fmData?.results?.albummatches &&
-          fmData.results.albummatches.album.map((album: any) => (
-            <List.Item
-              key={album.name + album.artist}
-              title={album.name}
-              description={album.artist}
-              // left={(props) => <List.Icon {...props} icon="album" />}
-            />
-          ))}
-      </List.Section>
+      <FlatList
+        data={fmData?.results?.albummatches.album}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.image[0]['#text']}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // // marginTop: StatusBar.currentHeight,
+  },
+  item: {
+    // backgroundColor: '#f9c2ff',
+    // height: 50,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 4,
+    // // marginHorizontal: 16,
+    // padding: 10,
+    flexDirection: 'row',
+  },
+  title: {
+    // fontSize: 24,
+  },
+});
